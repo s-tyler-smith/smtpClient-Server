92f05d300e085c86b0f69de097355c603ebd7a2b
s-tyler-smith <stephen.ty.smith@gmail.com>
2014-02-07 17:14:17 -0500

    works with error message added

--- comp431/HW3/src/client_side/SMTP2.java
+++ comp431/HW3/src/client_side/SMTP2.java
@@ -1,7 +1,6 @@
 package client_side;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,16 +10,15 @@ import java.util.Queue;
 public class SMTP2 {
 
 	private static ProtocolState currentState;
-	private static final String MAIL_FROM="MAIL FROM:";
-	private static final String RCPT_TO="RCPT TO:";
-	private static final String DATA="DATA";
-	
-	private static Queue<String> printQueue=new LinkedList<String>();
-	
+	private static final String MAIL_FROM = "MAIL FROM:";
+	private static final String RCPT_TO = "RCPT TO:";
+	private static final String DATA = "DATA";
+
+	private static Queue<String> printQueue = new LinkedList<String>();
 
 	// enum for maintaining state
 	private enum ProtocolState {
-		MAILFROMSTATE, RCPT_TOSTATE,REQ_DATA,SEND_DATA,END_DATA
+		MAILFROMSTATE, RCPT_TOSTATE, REQ_DATA, SEND_DATA, END_DATA, ERROR
 	};
 
 	public static void main(String[] args) throws IOException {
@@ -41,100 +39,117 @@ public class SMTP2 {
 		String nextResponseLine = null;
 
 		do {
-			
-			if((currentState==ProtocolState.REQ_DATA)){
-				
+			if (currentState == ProtocolState.ERROR) {
+				System.err.println("QUIT");
+				break;
+			}
+
+			if ((currentState == ProtocolState.REQ_DATA)) {
+
 				processFileInput(DATA);
-						
-			}else if(currentState==ProtocolState.END_DATA){
-				
-				if(printQueue.size()>0){
+
+			} else if (currentState == ProtocolState.END_DATA) {
+
+				if (printQueue.size() > 0) {
 					processFileInput(printQueue.poll());
 				}
-				
-			}else{
+
+			} else {
 				nextFileLine = fileBuffer.readLine();
 				processFileInput(nextFileLine);
 			}
-		
-			//printNextCommand();
-			
-		if (!(currentState == ProtocolState.SEND_DATA)) {
-			
-			nextResponseLine = responseBuffer.readLine();
-			
-			processServerResponse(nextResponseLine);
-		}
-			
+
+			// printNextCommand();
+
+			if (!(currentState == ProtocolState.SEND_DATA)) {
+
+				nextResponseLine = responseBuffer.readLine();
+
+				processServerResponse(nextResponseLine);
+			}
+
 		} while (nextFileLine != null && nextResponseLine != null);
 
 		fileBuffer.close();
 		responseBuffer.close();
 
 	}
-	private static void printNextCommand(){
-		if(printQueue.size()!=0){
-			System.err.println(printQueue.poll());
-			if(printQueue.size()!=0){
-				System.err.println(printQueue.poll());
-			}
-		}
-	}
 
 	private static void processFileInput(String line) {
-		//if(line==null)return;
-		//System.out.println(line);
-		if(currentState==ProtocolState.MAILFROMSTATE && line.startsWith("From: ")){
-			
-			System.err.println(MAIL_FROM+line.substring(line.indexOf(' ')));
-				
-		}else if(currentState==ProtocolState.RCPT_TOSTATE && line.startsWith("To: ")){
-			
-			System.err.println(RCPT_TO+line.substring(line.indexOf(' ')));
-			
-		}else if(currentState==ProtocolState.REQ_DATA){
-			
+		// if(line==null)return;
+		// System.out.println(line);
+		if (currentState == ProtocolState.MAILFROMSTATE
+				&& line.startsWith("From: ")) {
+
+			System.err.println(MAIL_FROM + line.substring(line.indexOf(' ')));
+
+		} else if (currentState == ProtocolState.RCPT_TOSTATE
+				&& line.startsWith("To: ")) {
+
+			System.err.println(RCPT_TO + line.substring(line.indexOf(' ')));
+
+		} else if (currentState == ProtocolState.REQ_DATA) {
+
 			System.err.println(line);
-			
-		}else if(currentState==ProtocolState.SEND_DATA && line==null){
+
+		} else if (currentState == ProtocolState.SEND_DATA && line == null) {
 			System.err.println(".");
-			currentState=ProtocolState.END_DATA;
-		
-		}else if(currentState==ProtocolState.SEND_DATA && line.startsWith("From: ")){
-			
-			currentState=ProtocolState.END_DATA;
-			
+			currentState = ProtocolState.END_DATA;
+
+		} else if (currentState == ProtocolState.SEND_DATA
+				&& line.startsWith("From: ")) {
+
+			currentState = ProtocolState.END_DATA;
+
 			System.err.println(".");
-			printQueue.add(MAIL_FROM+line.substring(line.indexOf(' ')));
-		
-		}else if(currentState==ProtocolState.SEND_DATA){
-			
+			printQueue.add(MAIL_FROM + line.substring(line.indexOf(' ')));
+
+		} else if (currentState == ProtocolState.SEND_DATA) {
+
 			System.err.println(line);
-				
-		}else if(currentState==ProtocolState.END_DATA){
-			
+
+		} else if (currentState == ProtocolState.END_DATA) {
+
 			System.err.println(line);
-			currentState=ProtocolState.MAILFROMSTATE;
+			currentState = ProtocolState.MAILFROMSTATE;
 		}
 
 	}
 
 	private static void processServerResponse(String response) {
-		if(response==null){
+		if (response == null) {
 			return;
 		}
-		if(currentState==ProtocolState.MAILFROMSTATE){
-			System.err.println(response);
-			currentState=ProtocolState.RCPT_TOSTATE;
-		}else if(currentState==ProtocolState.RCPT_TOSTATE){
-			System.err.println(response);
-			currentState=ProtocolState.REQ_DATA;
-		}else if(currentState==ProtocolState.REQ_DATA){
-			System.err.println(response);
-			currentState=ProtocolState.SEND_DATA;
-		}else if(currentState==ProtocolState.END_DATA){
-			System.err.println(response);
+		if (currentState == ProtocolState.MAILFROMSTATE) {
+			if (response.equals("250 OK")) {
+				System.err.println(response);
+				currentState = ProtocolState.RCPT_TOSTATE;
+			} else {
+				currentState = ProtocolState.ERROR;
+			}
+		} else if (currentState == ProtocolState.RCPT_TOSTATE) {
+			if (response.equals("250 OK")) {
+				System.err.println(response);
+				currentState = ProtocolState.REQ_DATA;
+			} else {
+				currentState = ProtocolState.ERROR;
+			}
+		} else if (currentState == ProtocolState.REQ_DATA) {
+			if (response.equals("354")) {
+				System.err.println(response);
+				currentState = ProtocolState.SEND_DATA;
+			} else {
+				currentState = ProtocolState.ERROR;
+			}
+
+		} else if (currentState == ProtocolState.END_DATA) {
+			if (response.equals("250 OK")) {
+				System.err.println(response);
+			} else {
+				currentState = ProtocolState.ERROR;
+			}
+
 		}
-		
+
 	}
 }
