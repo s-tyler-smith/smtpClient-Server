bf13da138ee88cef3880d33c0db937f1b188f325
s-tyler-smith <stephen.ty.smith@gmail.com>
2014-02-06 23:51:38 -0500

    added two buffered readers and sequence for reading

--- comp431/HW3/src/client_side/SMTP2.java
+++ comp431/HW3/src/client_side/SMTP2.java
@@ -4,32 +4,64 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class SMTP2 {
 
-	/**
-	 * @param args
-	 * 
-	 */
+	private static ProtocolState currentState;
+
+	// enum for maintaining state
+	private enum ProtocolState {
+		MAILFROMSTATE, RCPT_TOSTATE, DATA
+	};
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		BufferedReader myBuffer;
-		String nextLine = null;
-		myBuffer=new BufferedReader(new FileReader(args[0]));
-			do {
-				try {
-					nextLine = myBuffer.readLine();
-					System.out.println(nextLine);
-				} catch (IOException e) {
-					
-					e.printStackTrace();
-				}
-				
-				
-			} while (nextLine != null);
-			myBuffer.close();
+		currentState = ProtocolState.MAILFROMSTATE;
 
+		// BufferedReader fileBuffer=new BufferedReader(new
+		// FileReader(args[0]));
+		BufferedReader fileBuffer = new BufferedReader(new FileReader(
+				"forward/TONYbO@CS.txt"));
+
+		// buffer for reading input
+		BufferedReader responseBuffer = new BufferedReader(
+				new InputStreamReader(System.in));
+
+		String nextFileLine = null;
+		String nextResponseLine = null;
+
+		do {
+
+			try {
+
+				nextFileLine = fileBuffer.readLine();
+
+				processFileInput(nextFileLine);
+
+				if (!(currentState == ProtocolState.DATA)) {
+					nextResponseLine = responseBuffer.readLine();
+
+					processServerResponse(nextResponseLine);
+				}
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+		} while (nextFileLine != null && nextResponseLine != null);
+		fileBuffer.close();
+
+	}
+
+	private static void processFileInput(String line) {
+		System.out.println(line);
+
+	}
+
+	private static void processServerResponse(String response) {
+		System.out.println(response);
 	}
 
 }
