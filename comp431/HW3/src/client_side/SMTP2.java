325bba24099e0e5829aef347c5ed07492abaea03
s-tyler-smith <stephen.ty.smith@gmail.com>
2014-02-11 23:49:01 -0500

    Commented and ready to go

--- comp431/HW3/src/client_side/SMTP2.java
+++ comp431/HW3/src/client_side/SMTP2.java
@@ -15,10 +15,11 @@ public class SMTP2 {
 	private static final String DATA = "DATA";
 	private static final String QUIT="QUIT";
 
+	//strings to check the lines in the file for commands
 	private static final String FPATH = "From: <[^<>()\\.,\\\\:@;\\s\\[\\]\"]+@[a-zA-Z][a-zA-Z\\d]+(\\.{1}[a-zA-Z][a-zA-Z\\d]+)*>";
-
 	private static final String TPATH = "To: <[^<>()\\.,\\\\:@;\\s\\[\\]\"]+@[a-zA-Z][a-zA-Z\\d]+(\\.{1}[a-zA-Z][a-zA-Z\\d]+)*>";
 
+	//expected responses
 	private static final String OK_STATEMENT = "( |\t)*250[^\\d].*";
 	private static final String DATA_SEND_STATEMENT = "( |\t)*354[^\\d].*";
 
@@ -39,6 +40,7 @@ public class SMTP2 {
 
 	public static void main(String[] args) {
 
+		//check if no argument passed in
 		if ((args == null) || args.length<=0) {
 
 			endProgram(ProtocolState.ERROR);
@@ -51,9 +53,7 @@ public class SMTP2 {
 			// take in a single file as input
 			 fileBuffer=new BufferedReader(new
 			 FileReader(args[0]));
-			//fileBuffer = new BufferedReader(new FileReader(
-				//	"forward/TONYbO@CS.txt"));
-
+			 
 			// buffer for reading input
 			responseBuffer = new BufferedReader(
 					new InputStreamReader(System.in));
@@ -98,28 +98,36 @@ public class SMTP2 {
 				if (!(currentState == ProtocolState.SEND_DATA)) {
 
 					nextResponseLine = responseBuffer.readLine();
+					
+					//output server response on err
 					System.err.println(nextResponseLine);
-
+					
 					processServerResponse(nextResponseLine);
 				}
 
 			} while (nextFileLine != null && nextResponseLine != null);
 
-			// end the program
+			// end the program successfully
 			endProgram(ProtocolState.END_DATA);
 
 		} catch (Exception e) {
+			
+			//file wasn't found or an error creating
+			//the buffers has occurred
 			endProgram(ProtocolState.ERROR);
 		}
 	}
 
 	private static void processFileInput(String line) {
 
+		//Process file data depending 
+		//on the current state
 		switch (currentState) {
 
 		case MAILFROMSTATE:
+			
 			if (line.matches(FPATH)) {
-				// generate command
+				// generate  MAIL FROM: command
 				System.out.println(MAIL_FROM
 						+ line.substring(line.indexOf(' ')));
 			}
@@ -127,54 +135,60 @@ public class SMTP2 {
 			
 		case RCPT_TOSTATE:
 			if (line.matches(TPATH)) {
-				// generate command
+				// generate RCPT TO: command
 				System.out.println(RCPT_TO + line.substring(line.indexOf(' ')));
 			}
 			break;
 
 		case REQ_DATA:
+			
 			// create DATA command
 			System.out.println(DATA);
 
 			break;
-
+		//case where we are supposed to be printing the message body
+		//we have to check for end of file or next mail from message
 		case SEND_DATA:
 
+			//end of file 
 			if (line == null) {
 
 				currentState = ProtocolState.END_DATA;
 
 				System.out.println(".");
-
+			//another mail from command is detected
 			} else if (line.matches(FPATH)) {
 
 				currentState = ProtocolState.END_DATA;
 
 				System.out.println(".");
-
+				//save that command and continue
+				//I will print it out the next while loop iteration
 				printQueue.add(MAIL_FROM + line.substring(line.indexOf(' ')));
 
 			} else {
-
+				//normal case where are simply printing 
+				//data in the message body
 				System.out.println(line);
 			}
 			break;
 
 		case END_DATA:
-
+			//done sending data
 			System.out.println(line);
-
+			
+			//reset to mail from state
 			currentState = ProtocolState.MAILFROMSTATE;
 
 			break;
 
 		default:
-
 			break;
 		}
 	}
 
 	private static void processServerResponse(String response) {
+		//if null just stop
 		if (response == null) {
 			return;
 		}
@@ -182,7 +196,7 @@ public class SMTP2 {
 		switch (currentState) {
 
 		case MAILFROMSTATE:
-
+			//if response was good change state
 			if (response.matches(OK_STATEMENT)) {
 
 				currentState = ProtocolState.RCPT_TOSTATE;
@@ -207,7 +221,9 @@ public class SMTP2 {
 			break;
 
 		case REQ_DATA:
-
+			//transition to sending data state where the 
+			//message body gets printed out and we no longer
+			//need to get a server response
 			if (response.matches(DATA_SEND_STATEMENT)) {
 
 				currentState = ProtocolState.SEND_DATA;
@@ -219,7 +235,8 @@ public class SMTP2 {
 			break;
 
 		case END_DATA:
-
+			//if ending is anything but a 250 ok
+			//quit
 			if (!response.matches(OK_STATEMENT)) {
 
 				endProgram(ProtocolState.ERROR);
@@ -235,20 +252,20 @@ public class SMTP2 {
 	}
 
 	private static void endProgram(ProtocolState state) {
-
+		//an error has occurred somewhere in the program
 		if (state == ProtocolState.ERROR) {
-
+			
 			System.out.println(QUIT);
 
 			try {
-				
+				//close the file read buffer
 				fileBuffer.close();
 
 			} catch (Exception e) {
 			}
 
 			System.exit(1);
-
+		//exit with no error
 		} else {
 
 			System.out.println(QUIT);
